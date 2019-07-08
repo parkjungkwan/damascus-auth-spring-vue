346cc37881c2eb817dc1081985e96be97969b697
pakjkwan <pakjkwan@gmail.com>
2019-07-08 17:59:12 +0900

    17:58

---
+++ frontend2/src/reducers/todoReducers.jsx
@@ -0,0 +1,11 @@
+const todos = (state = {list:[]}, action) =>{
+    switch(action.type){
+        case ADD_TODO:
+            console.log('ADD_TODO')
+        default:
+            return state
+    }
+    return state
+}
+
+export default todos;

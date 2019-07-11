2eec44ebfd0d0852b83ffa42f8eef5eb76dde0f7
pakjkwan <pakjkwan@gmail.com>
2019-07-11 10:42:05 +0900

    10:42

---
+++ frontend4/src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function User({ user }){
+    return (
+        <div>
+            <h2>User 컴포넌트</h2>
+            <p></p>
+        </div>
+    )
+}
--- frontend4/src/index.js
+++ frontend4/src/index.js
@@ -3,24 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import * as reducers from './reducers';
+// import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import logger from 'redux-logger';
+// import * as reducers from './reducers';
 import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux'
+import createBrowserHistory from 'history/createBrowserHistory';
 
-const store = createStore(
-    combineReducers(reducers),
-    applyMiddleware(logger)
-)
+const history = createBrowserHistory();
+
+const store = createStore(history);
 
 ReactDOM.render(
     <Provider store={store}>
+        <ConnectedRouter history={history}>
         <App />
+        </ConnectedRouter>
     </Provider>,
     
     document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
